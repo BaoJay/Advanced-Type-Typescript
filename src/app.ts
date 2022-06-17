f28658dd68a5841c08e49d64e85a376147e3d441
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-17 17:03:08 +0700

    optional chaining

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,12 @@
-const userInputElement = document.querySelector('#user-input')! as HTMLInputElement;
-
-userInputElement.value = 'Hi there!!'
-
-// ======================================================
-interface Employee {
-    name: string;
-    startDate: Date;
-}
-
-interface ElevatedEmployee extends Admin, Employee {}
-
-const e1: ElevatedEmployee = {
+const = fetchUserData = {
+    id: 'u1',
     name: 'Bao',
-    privileges: ['create-server'],
-    startDate: new Date()
-}
-
-type Combinable = string | number;
-type Numberic = number | boolean;
-
-type Universal = Combinable & Numberic;
-
-
-function add(a: number, b: number): number;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: string, b: string): string;
-function add(a: Combinable, b: Combinable) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    } else {
-        return a + b;
+    job: {
+        title: 'CEO',
+        description: 'My own company'
     }
 }
-add(1,50)
-add(1, 'hoai')
-add('asgu','asd')
+//Access data by normal, if it’s available
+console.log(fetchUserData.job.title)
+// Access data by using optional chaining
+console.log(fetchUserData?.job?.title)
