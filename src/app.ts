c4b5d395241b090c1003d2beb49ba508641e171a
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-17 11:42:48 +0700

    dicriminated unions

--- src/app.ts
+++ src/app.ts
@@ -1,77 +1,25 @@
-interface Admin {
-    name: string;
-    privileges: string[];
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
 }
 
-interface Employee {
-    name: string;
-    startDate: Date;
+interface Horse {
+    type: 'horse';
+    runningSpeed: number;
 }
 
-interface ElevatedEmployee extends Admin, Employee {}
+type Animal = Bird | Horse;
 
-const e1: ElevatedEmployee = {
-    name: 'Bao',
-    privileges: ['create-server'],
-    startDate: new Date()
-}
-
-type Combinable = string | number;
-type Numberic = number | boolean;
-
-type Universal = Combinable & Numberic;
-
-// function add (a: Combinable, b: Combinable) {
-//     if (typeof a === 'string' || typeof b === 'string') {
-//         return a.toString() + b.toString();
-//     } else {
-//         return a + b;
-//     }
-// }
-
-type UnknownEmployee = Admin | Employee;
-
-function printEmployeeInformation (emp: UnknownEmployee) {
-    console.log('Name: ', emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ', emp.privileges);
+function useAnimal(animal: Animal) {
+    let speed;
+    switch(animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
     }
-    if ('startDate' in emp) {
-        console.log('Start Date: ', emp.startDate);
-    }
+    return speed
 }
 
-printEmployeeInformation(e1);
-printEmployeeInformation({name: 'Gia', startDate: new Date(),});
-
-class Car {
-    drive() {
-        console.log('Driving....')
-    }
-}
-
-class Truck {
-    drive() {
-        console.log('Driving a truck...')
-    }
-
-    loadCargo (amount: number) {
-        console.log('Loading cargo ...' + amount)
-    }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(15540);
-        
-    }
-}
-
-useVehicle(v1)
-useVehicle(v2)
+console.log(useAnimal({type: 'bird', flyingSpeed: 5000}))
