4ce0fd5493b96c1a10d2b446bc2b6bfc17200799
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-17 16:52:46 +0700

    function overloads

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,18 @@ type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
 
-function add (a: Combinable, b: Combinable) {
+
+function add(a: number, b: number): number;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString();
     } else {
         return a + b;
     }
 }
+add(1,50)
+add(1, 'hoai')
+add('asgu','asd')
